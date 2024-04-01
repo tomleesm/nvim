393a9d6d5494a5b7c49bf6b0cd85d07d2dff5785
Tom <tomleesm@gmail.com>
2024-04-01 10:28:32 +0800

    新增 neovim 設定
    - 安裝 lazy.nvim
    - 設定 Leader key ,
    - 安裝 theme flexoki
    - 安裝 nvim-neo-tree/neo-tree.nvim，設定 ,f 切換 Neotree
    - 顯示行數
    - 為了左邊的行數能有不同的顏色，所以顯示 cursor，line cursor line 只顯示左邊的數字
    - 前景文字改成白色，背景色改成黑色

---
+++ init.lua
@@ -0,0 +1,43 @@
+-- install lazy.nvim
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not (vim.uv or vim.loop).fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = "," -- Make sure to set `mapleader` before lazy so your mappings are correct
+vim.g.maplocalleader = "," -- Same for `maplocalleader`
+
+require("lazy").setup({
+  -- theme flexoki
+  {
+    "kepano/flexoki-neovim", name = "flexoki"
+  }, 
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons",
+      "MunifTanjim/nui.nvim",
+    }
+}
+})
+vim.cmd('colorscheme flexoki-dark')
+-- 顯示行數
+vim.o.number = true
+-- 為了左邊的行數能有不同的顏色，所以顯示 cursor line
+vim.o.cursorline = true
+-- cursor line 只顯示左邊的數字，預設是 both(=number,line)
+vim.o.cursorlineopt = "number"
+-- 前景文字改成白色，背景色改成黑色
+vim.api.nvim_set_hl(0, "Normal", { ctermfg=White,  ctermbg=Black })
+-- ,f 切換 Neotree
+vim.cmd('noremap ,f :Neotree toggle<CR>')
