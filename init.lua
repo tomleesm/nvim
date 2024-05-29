4e899080a313ac6bc37cebcb7934e31ec4025d79
Tom <tomleesm@gmail.com>
2024-05-30 00:43:59 +0800

    在 Insert mode 輸入兩個 ; 會在該行結尾加上 ; 後換行

--- init.lua
+++ init.lua
@@ -118,3 +118,7 @@ end
 if check_git_workspace() then
   vim.cmd("autocmd TextChanged,InsertLeave <buffer> silent write")
 end
+
+-- 在 Insert mode 輸入兩個 ; 會在該行結尾加上 ; 後換行，並維持 Insert mode
+-- 方便需要用分號結束 statement 的程式語言，例如 PHP, Java, C, Rust
+vim.keymap.set("i", ";;", "<ESC>A;<CR>")
