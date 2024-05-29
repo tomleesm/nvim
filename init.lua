77bec86c5fca0339027bf991d8b020c4f94fe9de
Tom <tomleesm@gmail.com>
2024-05-30 01:01:04 +0800

    設定 path 為 Laravel 的資料夾，方便用 :find 打開檔案

--- init.lua
+++ init.lua
@@ -122,3 +122,10 @@ end
 -- 在 Insert mode 輸入兩個 ; 會在該行結尾加上 ; 後換行，並維持 Insert mode
 -- 方便需要用分號結束 statement 的程式語言，例如 PHP, Java, C, Rust
 vim.keymap.set("i", ";;", "<ESC>A;<CR>")
+-- 設定 path 為 Laravel 的資料夾，方便用 :find 打開檔案
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "php",
+  callback = function(args)
+    vim.opt.path:append({ "app/**", "resources/**", "routes/**", "bootstrap/**", "database/**", "tests/**", "config/**", "storage/**" })
+  end
+})
