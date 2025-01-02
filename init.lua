72c256e43e4ce05fadb307a22f6095ed2291cd1b
Tom <tomleesm@gmail.com>
2025-01-02 18:22:35 +0800

    install plugin nvim-orgmode/orgmode

--- init.lua
+++ init.lua
@@ -37,6 +37,28 @@ require("lazy").setup({
     },
     {
       "farmergreg/vim-lastplace"
+    },
+    {
+      "nvim-treesitter/nvim-treesitter"
+    },
+    {
+      'nvim-orgmode/orgmode',
+      event = 'VeryLazy',
+      ft = { 'org' },
+      config = function()
+        -- Setup orgmode
+        require('orgmode').setup({
+          org_agenda_files = '~/orgfiles/**/*',
+          org_default_notes_file = '~/orgfiles/refile.org',
+        })
+
+        -- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
+        -- add ~org~ to ignore_install
+        -- require('nvim-treesitter.configs').setup({
+        --   ensure_installed = 'all',
+        --   ignore_install = { 'org' },
+        -- })
+      end,
     }
   }
 )
